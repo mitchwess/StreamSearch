df51a55b60fd41c61bb946015d558c46ec3d0fc3
zach south <zachsouth@zachs-MBP.neo.rr.com>
2021-11-26 15:50:02 -0500

    created front and backend

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # StreamSearch
+
+might have to run 'npm install --save cors' in backend folder to let frontend and backend work togethers
+
+'npm start' to run backend inside of the backend folder
+
+'npx start' to run frontend inside of frontend folder
---
+++ api/app.js
@@ -0,0 +1,44 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var cors=require("cors");//var to connect backend annd front end
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+testAPIRouter= require('./routes/testAPI');//path to test api backend file
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors()); //global meathod to connect backed and front end
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/testAPI',testAPIRouter); // www.  .com/testAPI will be a part of website
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
