3df8d578d0a27175420e669468d44c9f5bb89ef2
mitchwess <mitchwes180@gmail.com>
2021-12-07 17:57:32 -0500

    updated query.js

--- api/database_connetion/query.js
+++ api/database_connetion/query.js
@@ -12,8 +12,10 @@ class query_api{
 
   find_by_genre(option, genre){     //input string with name of one of the services as well as genre in a string
     pool.getConnection(function(err, connection){
-      if (err) throw err;
-      connection.query('SELECT * FROM ? WHERE listed_in LIKE "%?%"', [option, genre], function(error, results, fields){
+        if (err) throw err;
+        var sql = "SELECT * FROM " + option + " WHERE listed_in LIKE '%" + genre + "%'";
+        console.log(sql);
+        connection.query(sql, function(error, results, fields){
         console.log(results)
         connection.release();
         if (error) throw error;
@@ -31,18 +33,6 @@ class query_api{
 
   find_by_cast_member(){
 
-  }
- 
-
-  join_selected_tables(netfixOption, huluOption, primeOption, disneyOption){
-    pool.getConnection(function(err, connection){
-      if (err) throw err;
-      connection.query('SELECT * FROM ', [netflixOption, huluOption, disneyOption], function(error, results, fields){
-        console.log(results[0])
-        connection.release();
-        if (error) throw error;
-      });
-    });
   }
 }
 var tmp = new query_api;
